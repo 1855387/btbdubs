7f078bc824800873326c79a3e5d0a260f99efc5e
Kevin Do <54718041+kevin-dough@users.noreply.github.com>
2022-05-23 10:01:00 -0700

    login page

---
+++ src/main/java/com/nighthawk/intellijs/controllers/controller.java
@@ -0,0 +1,27 @@
+package com.nighthawk.intellijs.controllers;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+class controller {
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
