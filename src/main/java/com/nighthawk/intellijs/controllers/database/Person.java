5408ae5fd7124ccc1158ee36e372e4e04d2e6862
kylem <kylem31415@gmail.com>
2022-05-25 10:24:16 -0700

    Getters and setters for person object (email, name, password, dob)

--- src/main/java/com/nighthawk/intellijs/controllers/database/Person.java
+++ src/main/java/com/nighthawk/intellijs/controllers/database/Person.java
@@ -22,19 +22,35 @@ public class Person {
     private String password;
     private String dobString;
 
-    public String getFirstName() {
-        return firstName;
+    public String getEmail() {
+        return email;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    public String getLastName() {
-        return lastName;
+    public String getName() {
+        return name;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDob() {
+        return dobString;
+    }
+
+    public void setDob(String dobString) {
+        this.dobString = dobString;
     }
 }
