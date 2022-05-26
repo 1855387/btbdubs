3c3f54adc64520fbf336b3826b5395c3d0150d0b
kylem <kylem31415@gmail.com>
2022-05-26 09:44:11 -0700

    Fixed error with classes

--- src/main/java/com/nighthawk/intellijs/controllers/database/Person.java
+++ src/main/java/com/nighthawk/intellijs/controllers/database/Person.java
@@ -1,54 +1,54 @@
-package com.nighthawk.intellijs.controllers.database;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
-public class Person {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-
-    private String email;
-    private String name;
-    private String password;
-    private String dobString;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getDob() {
-        return dobString;
-    }
-
-    public void setDob(String dobString) {
-        this.dobString = dobString;
-    }
-}
+//package com.nighthawk.intellijs.controllers.database;
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//
+//@Entity
+//public class Person {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private long id;
+//
+//    private String email;
+//    private String name;
+//    private String password;
+//    private String dobString;
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getDob() {
+//        return dobString;
+//    }
+//
+//    public void setDob(String dobString) {
+//        this.dobString = dobString;
+//    }
+//}
--- src/main/java/com/nighthawk/intellijs/controllers/database/PersonRepository.java
+++ src/main/java/com/nighthawk/intellijs/controllers/database/PersonRepository.java
@@ -1,11 +1,11 @@
-package com.nighthawk.intellijs.controllers.database;
-
-import java.util.List;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
-//import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-public class PersonRepository {
-}
+//package com.nighthawk.intellijs.controllers.database;
+//
+//import java.util.List;
+//
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.data.repository.PagingAndSortingRepository;
+//import org.springframework.data.repository.query.Param;
+////import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+//
+//public class PersonRepository {
+//}
